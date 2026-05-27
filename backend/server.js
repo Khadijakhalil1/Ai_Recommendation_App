562c1a93282e99af3315f78d90bede751e790d03
Khadija khalil <Khadijakhalil2786@gmail.com>
2026-05-27 16:01:00 +0500

    update

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDb from './config/db.js'
 import dns from "node:dns/promises";
+import foodRouter from "./routes/foodroute.js";
 dns.setServers(["8.8.8.8","1.1.1.1"]);
 
 dotenv.config()
@@ -12,11 +13,16 @@ const port = process.env.PORT || 4000
 //midleware
 app.use(express.json())
 app.use(cors())
-app.get('/', (req, res)=>
-{
+
+connectDb()  // ← server ke andar nahi, bahar rakho
+
+
+//api endpoint
+app.use("/api/food",foodRouter)
+
+app.get('/', (req, res)=>{
     res.send('Hello World')
 })
-connectDb()  // ← server ke andar nahi, bahar rakho
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port} 🚀`)
